03d38fb9d6d4eeafbedc5432790207aff94bdbac
jaranya015 <151649534+jaranya015@users.noreply.github.com>
2024-01-12 09:28:01 +0700

    Update triangle_down.py

--- triangle_down.py
+++ triangle_down.py
@@ -1,3 +1,15 @@
+'''รับจำนวนแถวและสัญลักษณ์ จากนั้นวาดต้นไม้ยอดชี้ลง
+
+Enter the number of rows: 6
+Enter print symbol: *
+***********
+ *********
+  *******
+   *****
+    ***
+     *
+6 และ * คือข้อมูลที่ป้อนให้โปรแกรม'''
+
 enter = int(input("Enter the number of rows: "))
 enter2 = input("Enter print symbol: ")
 for i in range(enter, 0, -1):
