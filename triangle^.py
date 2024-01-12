145679e70cd0f2af5800e81f97925470ce6527cd
jaranya015 <151649534+jaranya015@users.noreply.github.com>
2024-01-12 09:05:45 +0700

    Update triangle^.py

--- triangle^.py
+++ triangle^.py
@@ -1,3 +1,14 @@
+'''รับจำนวนแถวและสัญลักษณ์ จากนั้นวาดต้นไม้ยอดชี้ขึ้น
+
+Enter the number of rows: 6
+Enter print symbol: *
+     *
+    ***
+   *****
+  *******
+ *********
+***********'''
+
 enter = int(input("Enter the number of rows: "))
 enter2 = input("Enter print symbol: ")
 for i in range(1,enter+1):
