0275dd50528326f92c6f84af404c5b66c9429b2c
jaranya015 <151649534+jaranya015@users.noreply.github.com>
2024-01-12 10:01:49 +0700

    Update encrytion.py

--- encrytion.py
+++ encrytion.py
@@ -1,3 +1,21 @@
+'''รับค่าตัวอักษรภาษาอังกฤษจากผู้ใช้มา 5 ตัวอักษร ได้ทั้งพิมพ์ใหญ่ และพิมพ์เล็ก
+แปลงตัวอักษรให้เป็นตัวอักษรในตําแหน่งเดียวกันเมื่อกลับด้าน
+ผลลัพธ์จะเป็นตัวเล็กทั้งหมด
+ABCDE แปลงเป็น zyxwv
+Gentle แปลงเป็น tvmgov
+ฝึก ใช้ความรู้ที่มี ห้ามใช้ความรู้เรื่องอื่นนะครับ :D
+Enter 5 characters: abcde
+Encryption is zyxwv
+abcde คืออักขระที่ป้อนให้โปรแกรม
+
+Enter 5 characters: zyxwv
+Encryption is abcde
+zyxwv คืออักขระที่ป้อนให้โปรแกรม
+
+Enter 5 characters: House
+Encryption is slfhv
+house คืออักขระที่ป้อนให้โปรแกรม'''
+
 def encryption(characters):
     dict_characters = {'a':'z', 'b':'y', 'c':'x', 'd':'w', 'e':'v', 'f':'u', 'g':'t', 'h':'s', 'i':'r', 'j':'q', 'k':'p', 'l':'o', 'm':'n', 'z':'a', 'y':'b', 'x':'c', 'w':'d', 'v':'e', 'u':'f', 't':'g', 's':'h', 'r':'i', 'q':'j', 'p':'k', 'o':'l', 'n':'m'}
     encrypted_text = ''.join([dict_characters.get(char, char) for char in characters])
