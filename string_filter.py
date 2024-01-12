8f3504c2e846c8062640a931e6607cf8c3b09d9a
jaranya015 <151649534+jaranya015@users.noreply.github.com>
2024-01-12 09:29:14 +0700

    Update string_filter.py

--- string_filter.py
+++ string_filter.py
@@ -1,7 +1,20 @@
+'''รับข้อความจากผู้ใช้
+รับคำที่ไม่ต้องการให้แสดงออกมา
+เปลี่ยนข้อความที่ไม่ต้องการให้แสดงเป็น * ตามจำนวนตัวอักษรของคำที่ไม่ต้องการให้แสดง
+Hello, World! ไม่อยากให้แสดง Hell → ****o, World!
+Love, Song ไม่อยากให้แสดง Love → ****, Song
+แสดงข้อความออกทางหน้าจอ
+ตัวอย่างโปรแกรม
+
+Enter text: Hello, World!
+Enter filter word: Hell
+Text after filter is "****o, World!"
+Hello, World! และ Hell คือข้อความที่ใส่ให้โปรแกรม
+
 enter = input("Enter text: ")
 enter2 = input("Enter filter word: ")
 encode_text = enter.replace(enter2 , '*'*len(enter2))
-print(f'Text after filter is "{encode_text}"')
+print(f'Text after filter is "{encode_text}"')'''
     
     
     
