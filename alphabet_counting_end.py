1bda590d2d3b261bb428601db9db4bf67b5ee062
jaranya015 <151649534+jaranya015@users.noreply.github.com>
2024-01-12 08:50:48 +0700

    Update alphabet_counting_end.py

--- alphabet_counting_end.py
+++ alphabet_counting_end.py
@@ -1,3 +1,59 @@
+'''เขียนโปรแกรมนับตัวอักษรจากข้อความที่ผู้ใช้ป้อนให้จนกว่าจะได้รับข้อความว่า 'end'
+
+รับข้อมูลจากผู้ใช้จนกว่าจะป้อนเฉพาะคำว่า end
+แปลงข้อความที่ผู้ใช้ป้อนให้เป็นตัวพิมพ์เล็กแล้วนับตัวอักษร
+นับเฉพาะตัวอักษรไม่นับเครื่องหมายอื่น
+แสดงข้อมูลเฉพาะตัวอักษรที่ที่ปรากฎในข้อความที่ผู้ใช้ป้อนโดยเรียงตามตัวอักษร
+ตัวอย่างการใช้งาน
+
+Enter string: Hello, World!
+Enter string: Hello -> Python \o/                              
+Enter string: end
+******************************
+*     Alphabet Counting      *
+******************************
+d 1
+e 2
+h 3
+l 5
+n 1
+o 5
+p 1
+r 1
+t 1
+w 1
+y 1
+******************************
+Enter string: Even though I know I am so in love with you
+Enter string: I’m not really sure that you feel the same way too
+Enter string: Ready for that moment, when my hearts broken again
+Enter string: Yeahh! Yeahh! Yeahh!
+Enter string: end
+******************************
+*     Alphabet Counting      *
+******************************
+a 13
+b 1
+d 1
+e 17
+f 2
+g 2
+h 14
+i 6
+k 2
+l 4
+m 6
+n 8
+o 12
+r 6
+s 4
+t 11
+u 4
+v 2
+w 4
+y 9
+******************************'''
+
 letter_count = {}
 while True:
     enter = input("Enter string: ")
