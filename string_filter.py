ee942505f6b53b790c1ff9678d6a0ae2b3920285
jaranya015 <151649534+jaranya015@users.noreply.github.com>
2024-01-16 18:01:57 +0700

    Update string_filter.py

--- string_filter.py
+++ string_filter.py
@@ -9,12 +9,12 @@ Love, Song ไม่อยากให้แสดง Love → ****, Song
 Enter text: Hello, World!
 Enter filter word: Hell
 Text after filter is "****o, World!"
-Hello, World! และ Hell คือข้อความที่ใส่ให้โปรแกรม
+Hello, World! และ Hell คือข้อความที่ใส่ให้โปรแกรม'''
 
 enter = input("Enter text: ")
 enter2 = input("Enter filter word: ")
 encode_text = enter.replace(enter2 , '*'*len(enter2))
-print(f'Text after filter is "{encode_text}"')'''
+print(f'Text after filter is "{encode_text}"')
     
     
     
